a8798bdf3e53902f8e4a9cbd8c666759f5f32230
Alex Kastritski <akastrytski@hotmail.com>
2023-11-30 11:05:32 +0300

    new prj

---
+++ demo-pet/src/main/java/com/example/demopet/DemoPetApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPetApplication.class, args);
+	}
+
+}
